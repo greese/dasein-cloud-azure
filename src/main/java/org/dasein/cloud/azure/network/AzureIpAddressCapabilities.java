fd960e5ef97e85e5c8413df90ca24c9205a23f4b
Andy Lyall <andy.lyall@enstratus.com>
2015-03-09 23:27:16 +0000

    Update license information

--- src/main/java/org/dasein/cloud/azure/network/AzureIpAddressCapabilities.java
+++ src/main/java/org/dasein/cloud/azure/network/AzureIpAddressCapabilities.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2013-2014 Dell, Inc
+ * Copyright (C) 2013-2015 Dell, Inc
  *
  * ====================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
